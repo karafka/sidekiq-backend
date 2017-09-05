a5f01b39a7984ab91abec45ade49911b3f1c1fe9
Maciej Mensfeld <maciej@mensfeld.pl>
2017-09-05 11:43:02 +0200

    cleanup

--- lib/karafka/extensions/sidekiq_attributes_map.rb
+++ lib/karafka/extensions/sidekiq_attributes_map.rb
@@ -2,13 +2,20 @@
 
 module Karafka
   module Extensions
+    # Additional Karafka::Attributes map topic attributes that can be used when worker
+    # is active and we use sidekiq backend
     module SidekiqAttributesMap
+      # Class methods that extend the attributes map class
       module ClassMethods
+        # Extra topic fields that we need to have to work with sidekiq backend
+        # @return [Array<Symbol>] available topic options
         def topic
           super + %i[interchanger worker]
         end
       end
 
+      # Prepends class methods into attributes map
+      # @param base [Class] class that we prepend to
       def self.prepended(base)
         class << base
           prepend ClassMethods
