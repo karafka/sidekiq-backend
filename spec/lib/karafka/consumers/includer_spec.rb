5e529d18673516917d98175f1b3553f128e7eb84
Maciej Mensfeld <maciej@mensfeld.pl>
2018-08-16 11:43:41 +0200

    fix includer specs

--- spec/lib/karafka/consumers/includer_spec.rb
+++ spec/lib/karafka/consumers/includer_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Karafka::Consumers::Includer do
-  subject(:includer) { described_class.new }
+  subject(:includer) { described_class.new(topic) }
 
   let(:consumer_class) { Class.new(Karafka::BaseConsumer) }
+  let(:consumer) { consumer_class.new(topic) }
   let(:features_scope) { Karafka::Consumers }
   let(:backends_scope) { Karafka::Backends }
   let(:topic) do
@@ -15,28 +16,26 @@ RSpec.describe Karafka::Consumers::Includer do
     )
   end
 
-  before { consumer_class.topic = topic }
-
-  describe 'inline with batch processing' do
+  describe 'inline with batch consuming' do
     let(:backend) { :inline }
     let(:batch_consuming) { true }
     let(:responder) { nil }
 
-    it { expect(consumer_class.include?(backends_scope::Inline)).to eq true }
-    it { expect(consumer_class.include?(backends_scope::Sidekiq)).to eq false }
-    it { expect(consumer_class.include?(features_scope::SingleParams)).to eq false }
-    it { expect(consumer_class.include?(features_scope::Responders)).to eq false }
+    it { expect(consumer.singleton_class.include?(backends_scope::Inline)).to eq true }
+    it { expect(consumer.singleton_class.include?(backends_scope::Sidekiq)).to eq false }
+    it { expect(consumer.singleton_class.include?(features_scope::SingleParams)).to eq false }
+    it { expect(consumer.singleton_class.include?(features_scope::Responders)).to eq false }
   end
 
-  describe 'inline without batch processing' do
+  describe 'inline without batch consuming' do
     let(:backend) { :inline }
     let(:batch_consuming) { false }
     let(:responder) { nil }
 
-    it { expect(consumer_class.include?(backends_scope::Inline)).to eq true }
-    it { expect(consumer_class.include?(backends_scope::Sidekiq)).to eq false }
-    it { expect(consumer_class.include?(features_scope::SingleParams)).to eq true }
-    it { expect(consumer_class.include?(features_scope::Responders)).to eq false }
+    it { expect(consumer.singleton_class.include?(backends_scope::Inline)).to eq true }
+    it { expect(consumer.singleton_class.include?(backends_scope::Sidekiq)).to eq false }
+    it { expect(consumer.singleton_class.include?(features_scope::SingleParams)).to eq true }
+    it { expect(consumer.singleton_class.include?(features_scope::Responders)).to eq false }
   end
 
   describe 'inline with responder' do
@@ -44,10 +43,10 @@ RSpec.describe Karafka::Consumers::Includer do
     let(:batch_consuming) { false }
     let(:responder) { Class.new }
 
-    it { expect(consumer_class.include?(backends_scope::Inline)).to eq true }
-    it { expect(consumer_class.include?(backends_scope::Sidekiq)).to eq false }
-    it { expect(consumer_class.include?(features_scope::SingleParams)).to eq true }
-    it { expect(consumer_class.include?(features_scope::Responders)).to eq true }
+    it { expect(consumer.singleton_class.include?(backends_scope::Inline)).to eq true }
+    it { expect(consumer.singleton_class.include?(backends_scope::Sidekiq)).to eq false }
+    it { expect(consumer.singleton_class.include?(features_scope::SingleParams)).to eq true }
+    it { expect(consumer.singleton_class.include?(features_scope::Responders)).to eq true }
   end
 
   describe 'sidekiq with responder' do
@@ -55,9 +54,9 @@ RSpec.describe Karafka::Consumers::Includer do
     let(:batch_consuming) { false }
     let(:responder) { Class.new }
 
-    it { expect(consumer_class.include?(backends_scope::Inline)).to eq false }
-    it { expect(consumer_class.include?(backends_scope::Sidekiq)).to eq true }
-    it { expect(consumer_class.include?(features_scope::SingleParams)).to eq true }
-    it { expect(consumer_class.include?(features_scope::Responders)).to eq true }
+    it { expect(consumer.singleton_class.include?(backends_scope::Inline)).to eq false }
+    it { expect(consumer.singleton_class.include?(backends_scope::Sidekiq)).to eq true }
+    it { expect(consumer.singleton_class.include?(features_scope::SingleParams)).to eq true }
+    it { expect(consumer.singleton_class.include?(features_scope::Responders)).to eq true }
   end
 end
