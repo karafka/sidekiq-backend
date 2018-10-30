2b2378f36f55c1bc695c966cee4b67791ba72ca5
Maciej Mensfeld <maciej@mensfeld.pl>
2018-10-30 14:59:53 +0100

    missing specs

--- spec/lib/karafka/extensions/params_builder_spec.rb
+++ spec/lib/karafka/extensions/params_builder_spec.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 RSpec.describe Karafka::Extensions::ParamsBuilder do
-  pending
+  describe '#from_hash' do
+    subject(:params) { Karafka::Params::Builders::Params.from_hash(hash, topic) }
+
+    let(:hash) { { 'key' => rand } }
+    let(:topic) { instance_double(Karafka::Routing::Topic, parser: Class.new) }
+
+    it { expect(params['parser']).to eq topic.parser }
+    it { expect(params['key']).to eq hash['key'] }
+    it { is_expected.to be_a(Karafka::Params::Params) }
+  end
 end
