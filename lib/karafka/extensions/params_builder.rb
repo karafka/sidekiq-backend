b505e641daeeb4e17ad2010e6205bab7c22ca035
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-06 17:31:17 +0200

    typos remarks

--- lib/karafka/extensions/params_builder.rb
+++ lib/karafka/extensions/params_builder.rb
@@ -4,7 +4,7 @@ module Karafka
   module Extensions
     # Extension for rebuilding params from a hash
     module ParamsBuilder
-      # Buils params from a hash
+      # Builds params from a hash
       # @param hash [Hash] hash with params details
       # @param topic [Karafka::Routing::Topic] topic for which we build the params
       # @return [Karafka::Params::Params] built params
