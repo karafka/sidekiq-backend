40cb154fc1c81bb5becd952efd5bce99dad15111
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-10 12:23:44 +0200

    #26

--- lib/karafka/instrumentation/stdout_listener.rb
+++ lib/karafka/instrumentation/stdout_listener.rb
@@ -5,25 +5,23 @@ module Karafka
   module Instrumentation
     # Additional methods for listener that listen on instrumentation related to the Sidekiq
     # backend of Karafka
-    module StdoutListener
-      class << self
-        # Logs info about scheduling of a certain dataset with a Sidekiq backend
-        # @param event [Dry::Events::Event] event details including payload
-        def on_backends_sidekiq_process(event)
-          count = event[:caller].send(:params_batch).to_a.size
-          topic = event[:caller].topic.name
-          time = event[:time]
-          info "Scheduling of #{count} messages to Sidekiq on topic #{topic} took #{time} ms"
-        end
+    class StdoutListener
+      # Logs info about scheduling of a certain dataset with a Sidekiq backend
+      # @param event [Dry::Events::Event] event details including payload
+      def on_backends_sidekiq_process(event)
+        count = event[:caller].send(:params_batch).to_a.size
+        topic = event[:caller].topic.name
+        time = event[:time]
+        info "Scheduling of #{count} messages to Sidekiq on topic #{topic} took #{time} ms"
+      end
 
-        # Logs ino about processing certain events with a given Sidekiq worker
-        # @param event [Dry::Events::Event] event details including payload
-        def on_backends_sidekiq_base_worker_perform(event)
-          count = event[:consumer].send(:params_batch).to_a.size
-          topic = event[:consumer].topic.name
-          time = event[:time]
-          info "Sidekiq processing of topic #{topic} with #{count} messages took #{time} ms"
-        end
+      # Logs ino about processing certain events with a given Sidekiq worker
+      # @param event [Dry::Events::Event] event details including payload
+      def on_backends_sidekiq_base_worker_perform(event)
+        count = event[:consumer].send(:params_batch).to_a.size
+        topic = event[:consumer].topic.name
+        time = event[:time]
+        info "Sidekiq processing of topic #{topic} with #{count} messages took #{time} ms"
       end
     end
   end
