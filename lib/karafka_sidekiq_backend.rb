820193ce7cbfee768f233caeb1d626b2a3b6aef2
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-30 18:20:35 +0200

    zeitwerk integration

---
+++ lib/karafka_sidekiq_backend.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# @note Active Support is already included since Karafka is using it directly so no need
+#   to require it again in the gemspec, etc
+%w[
+  active_support/core_ext/class/subclasses
+  karafka
+  sidekiq
+].each(&method(:require))
+
+Zeitwerk::Loader
+  .for_gem
+  .tap { |loader| loader.ignore("#{__dir__}/karafka_sidekiq_backend.rb") }
+  .tap { |loader| loader.ignore("#{__dir__}/karafka-sidekiq-backend.rb") }
+  .tap(&:setup)
+  .tap(&:eager_load)
+  .tap { |loader| loader.preload('lib/') }
+
+Karafka::Params::Builders::Params.extend(Karafka::Extensions::ParamsBuilder)
+Karafka::Params::Builders::ParamsBatch.extend(Karafka::Extensions::ParamsBatchBuilder)
+Karafka::Params::Builders::Metadata.extend(Karafka::Extensions::MetadataBuilder)
+Karafka::Routing::Topic.include(Karafka::Extensions::SidekiqTopicAttributes)
+Karafka::AttributesMap.prepend(Karafka::Extensions::SidekiqAttributesMap)
+
+# Register internal events for instrumentation
+%w[
+  backends.sidekiq.process
+  backends.sidekiq.base_worker.perform
+].each(&Karafka.monitor.method(:register_event))
